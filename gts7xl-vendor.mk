13dc12afd7bef2a92463317a4fb54bf13bdc11bd
Atakan <ata.kaner03@gmail.com>
2025-08-02 09:06:50 +0300

    gts7xl: Ship missing thermal config
    08-01 19:02:14.453  1054  1054 E SehThermal-HAL: /vendor/etc/btcon.json was not found
    
    Change-Id: Ic6740777ac394b68c4dc947310c626a8dfde7322

--- gts7xl-vendor.mk
+++ gts7xl-vendor.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gts7xl/proprietary/vendor/etc/audconf/SLK/Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/SLK/Headset_cal.acdb \
     vendor/samsung/gts7xl/proprietary/vendor/etc/audconf/SLK/Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/SLK/Speaker_cal.acdb \
     vendor/samsung/gts7xl/proprietary/vendor/etc/audconf/SLK/workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/audconf/SLK/workspaceFile.qwsp \
+    vendor/samsung/gts7xl/proprietary/vendor/etc/btcon.json:$(TARGET_COPY_OUT_VENDOR)/etc/btcon.json \
     vendor/samsung/gts7xl/proprietary/vendor/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     vendor/samsung/gts7xl/proprietary/vendor/etc/gnss_antenna_info.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss_antenna_info.conf \
     vendor/samsung/gts7xl/proprietary/vendor/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
